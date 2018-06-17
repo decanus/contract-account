28659b1ceadca33fa417a7359f34b5b11d3670fb
decanus <dean.eigenmann@icloud.com>
2018-06-17 12:46:16 +0200

    started account

---
+++ contracts/Account.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.18;
+
+contract Account {
+
+    mapping (address => uint) public group;
+    mapping (uint => bool) public valueTransferEnabled;
+
+    function execute(address destination, uint value, bytes data) external {
+        if (value > 0) {
+            require(isValueTransferEnabled(msg.sender));
+        }
+
+        // @todo do data and destination checks
+
+        call(destination, value, data);
+    }
+
+    function isValueTransferEnabled(address sender) public view returns (bool) {
+        return valueTransferEnabled[group[sender]];
+    }
+
+    function call(address destination, uint value, bytes data) internal {
+
+        uint length = data.length;
+
+        assembly {
+            let x := mload(0x40)
+            let d := add(data, 32)
+            result := call(
+                sub(gas, 34710),
+                destination,
+                value,
+                d,
+                length,
+                x,
+                0
+            )
+        }
+    }
+
+}
